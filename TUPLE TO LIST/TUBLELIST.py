864afaf222db85ce23fabf23113c206c1539b070
chowdam89 <venturevideo45@gmail.com>
2026-05-03 18:40:14 +0530

    doen

---
+++ TUPLE TO LIST/TUBLELIST.py
@@ -0,0 +1,7 @@
+
+languages_tuple = ("Python", "JavaScript", "C++")
+languages_list = list(languages_tuple)
+languages_list.append("HTML, CSS , JS")
+languages_list[1] = "TypeScript"
+print("Original Tuple:", languages_tuple) 
+print("Modified List:", languages_list)   
