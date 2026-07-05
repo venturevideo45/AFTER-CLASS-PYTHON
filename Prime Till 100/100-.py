8beeff7626d6cd3fda2b5efd12f2498e81ee2834
chowdam89 <venturevideo45@gmail.com>
2026-07-05 18:02:17 +0530

    same as before

---
+++ Prime Till 100/100-.py
@@ -0,0 +1,15 @@
+def SieveofEratosthenes(num):
+    prime = [True for i in range(num + 1)]
+    p = 2
+    while (p * p <= num):
+        if (prime[p] == True):
+            for i in range(p * p, num + 1, p):
+                prime[i] = False
+        p += 1
+    for p in range(2, num):
+        if prime[p]:
+            print(p)
+
+num = 100
+print("The prime numbers up to", num, "are:")
+SieveofEratosthenes(num)
